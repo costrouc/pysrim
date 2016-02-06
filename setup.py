4c6455219b5eff0e81727cea04fcb3f9f7a99e6f
Christopher Ostrouchov <chris.ostrouchov@gmail.com>
2016-02-06 13:33:12 -0500

    Changes testing (add arguments) + moved testing dir

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+        
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='srim',
@@ -17,5 +31,6 @@ setup(
     setup_requires=['pytest-runner'],
     install_requires=['pyyaml'],
     tests_require=['pytest', 'pytest-mock'],
+    cmdclass = {'test': PyTest},
     # scripts=['scripts/']       
 )
