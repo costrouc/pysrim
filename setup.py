d7ec74a5aea3f1365eca84760255b3bf722ac6f7
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2017-08-11 11:25:49 -0400

    bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-version='0.1.4'
+version='0.1.5'
 setup(
     name='pysrim',
     version=version,
